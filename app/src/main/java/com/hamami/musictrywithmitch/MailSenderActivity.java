c6e151fe054a0e9bf600390d97ba9ddca077ef17
Michael Hamami <32095059+kosm10@users.noreply.github.com>
2019-04-15 14:46:58 +0300

    Commit: Message need to save the new queue position. need to finish code the shuffle need to finish code the getting from music folder need to think about get information from developer need to finish to send mails to developer

---
+++ app/src/main/java/com/hamami/musictrywithmitch/MailSenderActivity.java
@@ -0,0 +1,35 @@
+package com.hamami.musictrywithmitch;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MailSenderActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//
+//        final Button send = (Button) this.findViewById(R.id.send);
+//        send.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View v) {
+//                try {
+//                    GMailSender sender = new GMailSender("username@gmail.com", "password");
+//                    sender.sendMail("This is Subject",
+//                            "This is Body",
+//                            "user@gmail.com",
+//                            "user@yahoo.com");
+//                } catch (Exception e) {
+//                    Log.e("SendMail", e.getMessage(), e);
+//                }
+//
+//            }
+//        });
+
+    }
+}
